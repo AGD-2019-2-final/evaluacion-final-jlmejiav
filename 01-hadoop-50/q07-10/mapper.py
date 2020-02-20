d62bef5f282b1bf8f61a3abb5e98cf848e277595
Juan Luis Mejía Villa <jlmejiav@unal.edu.co>
2020-02-20 13:44:59 -0500

    aaaa

--- 01-hadoop-50/q07-10/mapper.py
+++ 01-hadoop-50/q07-10/mapper.py
@@ -2,19 +2,29 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
 if __name__ == "__main__":
+    class Mapper:
+    
+        def __init__(self, stream):       
+            self.stream = stream
 
-    ##
-    ## itera sobre cada linea de codigo recibida
-    ## a traves del flujo de entrada
-    ##
-    for line in sys.stdin:
+        def emit(self,claves,key,fecha,numero):      
+            sys.stdout.write("{}\t{}\t{}\t{}\n".format(claves,key,fecha,numero))
 
-        ##
-        ## genera las tuplas palabra \tabulador 1
-        ## ya que es un conteo de palabras
-        ##
-        word = line.split("   ")[0]
-        value = int(line.split("   ")[2])
-        value = "{:03.0f}".format(value)
-        sys.stdout.write("{},{},{}".format(word,value,line))
+        def map(self):
+            for word in self:
+                indice = word.split()[0]
+                fecha = word.split()[1]
+                numero = word.split()[2]
+                # claves creadas para que el combiner interno de hadoop
+                #  se lo tire organizado al reduce
+                claves = '{},{:0>4}'.format(indice,numero)
+                self.emit(claves=claves,key=indice,fecha=fecha,numero=numero)        
+
+        def __iter__(self):
+            for line in self.stream:                             
+                yield line
+                
+    mapper = Mapper(sys.stdin)  
+    mapper.map()
--- 01-hadoop-50/q07-10/reducer.py
+++ 01-hadoop-50/q07-10/reducer.py
@@ -2,12 +2,27 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
-if __name__ == '__main__':
+import itertools
+from operator import itemgetter
 
-    ##
-    ## cada linea de texto recibida es una
-    ## entrada clave \tabulador valor
-    ##
-    for line in sys.stdin:
-        _, _, val = line.split(",")
-        sys.stdout.write("{}".format(val))
+if __name__ == '__main__':
+    class Reducer:    
+        def __init__(self, stream):
+            self.stream = stream
+
+        def emit(self, key):
+            sys.stdout.write("{}\n".format(key)) 
+
+        def reduce(self):           
+            for i in self:
+                i = i.replace('\n','').split('\t')[1:]
+                i = "    ".join(i)
+                self.emit(key=i)
+            
+        def __iter__(self):
+
+            for line in self.stream:               
+                yield (line)
+                
+    reducer = Reducer(sys.stdin)
+    reducer.reduce()
